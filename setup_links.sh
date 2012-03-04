8d28e0d4c9d4655d8f1dd8b4247806648b699fe6
Brian Gamble <cc@theclearcoder.com>
2012-03-04 11:59:45 -0600

    Initial version of tmux.conf based on configuration from the pragmatic programms tmux book

--- setup_links.sh
+++ setup_links.sh
@@ -5,3 +5,4 @@ ln -nfs ~/bin/dotfiles/zshenv ~/.zshenv
 ln -nfs ~/bin/dotfiles/vim ~/.vim
 ln -nfs ~/bin/dotfiles/vim/vimrc ~/.vimrc
 ln -nfs ~/bin/dotfiles/vim/gvimrc ~/.gvimrc
+ln -nfs ~/bin/dotfiles/tmux.conf ~/.tmux.conf
