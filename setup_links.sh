9f47705e886adbc46ba6bf6f11ce14467969eb42
Brian Gamble <cc@theclearcoder.com>
2015-09-30 16:14:37 -0500

    Add vim swap files to ag ignore

--- setup_links.sh
+++ setup_links.sh
@@ -5,6 +5,7 @@ ln -nfs ~/bin/dotfiles/zshenv ~/.zshenv
 ln -nfs ~/bin/dotfiles/vim ~/.vim
 ln -nfs ~/bin/dotfiles/vim/vimrc ~/.vimrc
 ln -nfs ~/bin/dotfiles/vim/gvimrc ~/.gvimrc
+ln -nfs ~/bin/dotfiles/vim/agignore ~/.agignore
 ln -nfs ~/bin/dotfiles/tmux.conf ~/.tmux.conf
 ln -nfs ~/bin/dotfiles/tm_properties ~/.tm_properties
 ln -nfs ~/bin/dotfiles/gitconfig ~/.gitconfig
