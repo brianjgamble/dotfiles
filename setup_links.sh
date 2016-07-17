585d42cbbedbe3f4d224e7bfa9504366a9136c48
Brian Gamble <cc@theclearcoder.com>
2016-07-17 11:46:40 -0500

    Switching from Texmate 2 to Brackets

--- setup_links.sh
+++ setup_links.sh
@@ -6,5 +6,4 @@ ln -nfs ~/bin/dotfiles/vim/vimrc ~/.vimrc
 ln -nfs ~/bin/dotfiles/vim/gvimrc ~/.gvimrc
 ln -nfs ~/bin/dotfiles/vim/agignore ~/.agignore
 ln -nfs ~/bin/dotfiles/tmux.conf ~/.tmux.conf
-ln -nfs ~/bin/dotfiles/tm_properties ~/.tm_properties
 ln -nfs ~/bin/dotfiles/gitconfig ~/.gitconfig
