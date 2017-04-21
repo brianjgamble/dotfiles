dd9fdefac558f3edefe0bd3a88f8b24677117cf9
Brian Gamble <cc@theclearcoder.com>
2017-04-21 16:58:30 -0500

    Add global gitignore for things like VIM swap files, etc.

--- setup_links.sh
+++ setup_links.sh
@@ -8,4 +8,5 @@ ln -nfs ~/bin/dotfiles/vim/agignore ~/.agignore
 ln -nfs ~/bin/dotfiles/tmux.conf ~/.tmux.conf
 ln -nfs ~/bin/dotfiles/tm_properties ~/.tm_properties
 ln -nfs ~/bin/dotfiles/gitconfig ~/.gitconfig
+ln -nfs ~/bin/dotfiles/gitignore ~/.gitignore
 ln -nfs ~/bin/dotfiles/misc/audio_links.m3u ~/audio_links.m3u
