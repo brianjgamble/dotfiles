08d847e2c0c1cb2c855e340b94549c96d8ef0865
Brian Gamble <brianjgamble@users.noreply.github.com>
2021-06-09 22:47:09 -0500

    Add homebrew bin path to fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,11 @@
+switch (uname -m)
+  case arm64
+    set BREW_PREFIX /opt/homebrew
+  case '*'
+    set BREW_PREFIX /usr/local
+end
+set PATH "$BREW_PREFIX/bin" $PATH
+
 source (brew --prefix asdf)/asdf.fish
 
 if test -e ~/.config/fish/local.fish
