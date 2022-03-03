cc8a28a30a2840085423ea8a1d9db50590579d62
Brian Gamble <brianjgamble@users.noreply.github.com>
2022-03-02 22:34:42 -0600

    Release build for neovim

--- misc/install_neovim.sh
+++ misc/install_neovim.sh
@@ -1,4 +1,4 @@
 rm -r build/  # clear the CMake cache
-make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/local"
+make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/local -DCMAKE_BUILD_TYPE=Release"
 make install
 export PATH="$HOME/neovim/bin:$PATH"
