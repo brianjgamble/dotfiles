bee2a5a3b93b6ccbdba96508ca68eebdb03b31a2
Brian Gamble <brianjgamble@users.noreply.github.com>
2022-04-01 20:48:25 -0500

    Add themer color js

---
+++ themer/themer-colors-gentledark.js
@@ -0,0 +1,31 @@
+module.exports.colors = {
+  dark: {
+
+    // Colors can be defined in any valid CSS format.
+
+    // accent0-7 should be the main accent colors of your theme. See the table
+    // in the "Color mappings" section for how the colors will be used in your
+    // new themes.
+    accent0: '#BD6065',
+    accent1: '#D49454',
+    accent2: '#AC9F2A',
+    accent3: '#4BA495',
+    accent4: '#80BECB',
+    accent5: '#879499',
+    accent6: '#B95AA0',
+    accent7: '#6F8EB2',
+
+    // shade0-7 should be shades of the same hue, with shade0 being the
+    // background and shade7 being the foreground. If you omit the
+    // intermediate shades (1 through 6), they will be calculated automatically
+    // for you.
+    shade0: '#132230',
+    shade1: '#1C3649',
+    shade2: '#3E76A0',
+    shade3: '#4787B8',
+    shade4: '#C26FDB',
+    shade5: '#7FACCE',
+    shade6: '#9FA9AD',
+    shade7: '#B8B7BA'
+  }
+};
