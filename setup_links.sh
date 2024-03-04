c0bd2141a2ce9cc055482c8fd12ebe3b94ffa8d2
Brian Gamble <brianjgamble@users.noreply.github.com>
2024-03-03 22:18:05 -0600

    Cleanup files

--- setup_links.sh
+++ setup_links.sh
@@ -5,11 +5,8 @@ ln -nfs ~/bin/dotfiles/vim ~/.vim
 ln -nfs ~/bin/dotfiles/vim/vimrc ~/.vimrc
 ln -nfs ~/bin/dotfiles/vim/gvimrc ~/.gvimrc
 ln -nfs ~/bin/dotfiles/vim/agignore ~/.agignore
-ln -nfs ~/bin/dotfiles/tmux.conf ~/.tmux.conf
-ln -nfs ~/bin/dotfiles/tm_properties ~/.tm_properties
 ln -nfs ~/bin/dotfiles/gitconfig ~/.gitconfig
 ln -nfs ~/bin/dotfiles/gitignore ~/.gitignore
-ln -nfs ~/bin/dotfiles/misc/audio_links.m3u ~/audio_links.m3u
 
 mkdir -p ~/.config
 ln -nfs ~/bin/dotfiles/fish ~/.config/fish
