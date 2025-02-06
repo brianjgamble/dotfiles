5a51ad887eb6aee03a1ec5c3ece48aecc2eadcde
Brian Gamble <brianjgamble@users.noreply.github.com>
2025-02-05 22:21:41 -0600

    Add asdf shims to the path

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,19 @@
 set FLYCTL "/opt/homebrew/share/fish/vendor_completions.d/flyctl.fish"
 
+# add homebrew to path
 set PATH "/opt/homebrew/bin" $PATH
 
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
+# add asdf shims to path
+if test -z $ASDF_DATA_DIR
+  set _asdf_shims "$HOME/.asdf/shims"
+else
+  set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+set -gx --prepend PATH $_asdf_shims
+
+set --erase _asdf_shims
+
 source /opt/homebrew/share/fish/vendor_completions.d/asdf.fish
 source /opt/homebrew/share/fish/vendor_completions.d/brew.fish
 
