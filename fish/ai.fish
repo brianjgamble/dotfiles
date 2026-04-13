e5f6a465b39a8577b0fb94facfdcc58988631981
Brian Gamble <brianjgamble@users.noreply.github.com>
2026-04-12 23:30:12 -0500

    Update for the AI models I'm using

--- fish/ai.fish
+++ fish/ai.fish
@@ -1,18 +1,3 @@
-set -gx OLLAMA_KEEP_ALIVE "10m"
-
-# Updates all installed Ollama models
-function update_ollama
-  # Get model names, skipping the header line
-  set models (ollama list | tail -n +2 | awk '{print $1}')
-
-  for model in $models
-   echo "Checking for updates: $model"
-   ollama pull $model
-  end
-
-  echo "Done! All models processed."
-end
-
 # Gets a list of files in a directory and copies them to the clipboard to use in an AI prompt
 function ai_files
   set dir $argv[1]
@@ -42,4 +27,22 @@ function ai_files
   end | pbcopy
 
   echo "Copied files from '$dir' to clipboard (max $limit files)."
+end
+
+# Autocomplete
+function ai_auto
+  cd sandbox/llm/llama.cpp/build/bin
+  ./llama-server -hf ggml-org/Qwen2.5-Coder-1.5B-Q8_0-GGUF:Q8_0 --port 8081 -c 4096 --flash-attn on
+end
+
+# Gemma 4B
+function ai_gemma
+  cd sandbox/llm/llama.cpp/build/bin
+  ./llama-server -hf ggml-org/gemma-4-E4B-it-GGUF:Q8_0 --port 8080 -c 32768 --flash-attn on --no-mmap --jinja
+end
+
+# Qwen 14B
+function ai_qwen
+  cd sandbox/llm/llama.cpp/build/bin
+  ./llama-server -hf Qwen/Qwen2.5-Coder-14B-Instruct-GGUF:Q5_K_M --port 8080 -c 32768 --flash-attn on --no-mmap --jinja
 end
